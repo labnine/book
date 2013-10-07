888083b71ab552535e2570d5c3adafbd7fe70307
Mike Mitchel <mike@lab9inc.com>
2013-10-07 00:06:22 -0500

    sortme updated

--- sortme.rb
+++ sortme.rb
@@ -1,36 +1,48 @@
+require 'pry'
 #sort a list of words into alphabetical order
 
 #unsorted list
-unsorted = ["nancy", "apple", "Peter", "jesus", "pest", "thirsty", "Ball", "Thirteen", "zounds", "nexus"] 
-
-#sorted list
-sorted = []
-
+unsorted = ["nancy", "apple", "Peter", "jesus", "pest", "thirsty", "Ball", "pest", "Thirteen", "zounds"] 
+#unsorted = ["nancy", "Apple", "hot"]
 # code to sort it out
 
 #wrapper method
 
 def sort some_array
-	
-	recursive_sort some_array, []
+	sorted_array = []
+	recursive_sort some_array, sorted_array
 end
 
 #compare 2 elements of unsorted array, keeping the lesser until out of elements
 # pop/push to sorted, repeat recursively until unsorted array is empty
 
-def recursive_sort unsorted_array, sorted_array 
+def recursive_sort unsorted_array, sorted_array
 	u = unsorted_array
 	s = sorted_array
-	u.each do |e|
-		if e > u[e.index+1]
-			s += u.slice[e.index]
-		end
-	end
-	puts u
-	puts s
 
+  lowest = u.last
+
+
+
+  # test 2 elements, keep the lowest
+  u.each do |elem| 
+    if elem.downcase < lowest.downcase
+
+
+      lowest = elem
+      
+    end
+
+  end
+  s << lowest
+  u.delete_at(u.index(lowest)) 
+  
+  if u.empty? == false
+    recursive_sort(u, s)
+  else
+    s
+  end
 end
 	
 
-
-sort unsorted
+puts sort unsorted
